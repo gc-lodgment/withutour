5a1440789abd0b19d1268ec2bef9e6dc01677565
s-rainkin <45417223+s-rainkin@users.noreply.github.com>
2019-03-08 11:04:43 +0900

    추가업데이트 - 1차
    1. 텍스트 및 타이틀 수정
    2. 고객사 안내 추가 및 하단 파트너 권유 추가
    3. 자세한 시설 안내보기 팝업 추가
    4. 모바일 상품메뉴 추가

--- static/js/init.js
+++ static/js/init.js
@@ -1,6 +1,7 @@
 $(function(){
     //함수 실행
     tListFn();
+    popFn();
     
     //rwing버튼 위치
     $(window).scroll(function(){
@@ -15,11 +16,7 @@ $(function(){
     
     //버튼 클릭시 단체문의로 이동.
     $('.move-group').on('click', function(){
-        $('html, body').stop().animate({
-            //scrollTop: $($_anchor.attr('href')).offset().top - 150
-            scrollTop: ($("#etc").offset().top)
-        }, 800);
-        event.preventDefault();
+        groupMove();
     });
 });
 
@@ -45,4 +42,63 @@ function tListFn(){
         }
         
     });
+}
+
+// 팝업 인터렉션 
+function popFn(){
+    var btnPt, groupArr, ths;
+    var pop = $('.group-pop');
+    
+    //open popup
+    $('.btn-detail').on('click', function(){
+        if( $(this).hasClass('yet') ){
+           //console.log('yet')
+        }else{
+            btnPt = $(this).parents('.group-list').attr('class');
+            groupArr = btnPt.split(' ');
+            //console.log(groupArr[2]);
+            
+            pop.show();
+            pop.find('.pop-list.'+groupArr[2]).show();
+            
+            
+            $('html').css({'overflow': 'hidden', 'height': '100%'});
+            $(pop).on('scroll', function(event) { 
+                event.preventDefault();     
+                event.stopPropagation();     
+                return false; 
+            });
+        }
+    });
+    
+    //close popup
+    $('.btn-close').on('click', function(){
+        ths = $(this);
+        closePop(ths);
+    });
+    
+    //닫고 단체문의로 이동
+    $('.group-pop .btn-group').on('click', function(){
+        ths = $(this);
+        closePop(ths);
+        groupMove();
+    });
+    
+}
+
+//pop close function
+function closePop(ths){
+    ths.parents('.pop-list').hide().parents('.group-pop').hide();
+
+    $('html').css({'overflow': 'auto', 'height': '100%'});
+    ths.parents('.group-pop').off('scroll');
+}
+
+//단체문의로 이동
+function groupMove(){
+    $('html, body').stop().animate({
+        //scrollTop: $($_anchor.attr('href')).offset().top - 150
+        scrollTop: ($("#etc").offset().top)
+    }, 800);
+    event.preventDefault();
 }
