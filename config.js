0a6232a9d762070c911816c4b448645d58a86e0b
som <d.sssom13@gmail.com>
2019-09-19 10:19:45 +0900

    js update

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = function() {
 
 	var source       = 'src',
 		development  = 'dist',
+		test 		 = 'test',
 		remove       = ['.sass-cache', 'dist'],
 
 		// 템플릿 경로
@@ -11,45 +12,60 @@ module.exports = function() {
 			dest : development + '/views',
 			src_m  : source + '/template_m/**/!(_)*.html',
 			parts_m : source + '/template_m/**/_*.html',
-			dest_m : development + '/views_m'
+			dest_m : development + '/views_m',
 		},
 
 		// Sass 경로
 		sass = {
-			src : source + '/sass/**/*.{scss,sass}',
-			// src       : source + '/sass/**',
-			dest: development + '/static/css/renew'
+			src: source + '/sass/**/!(_)*.{scss,sass}',
+			parts: source + '/sass/**/_*.{scss,sass}',
+			dest      : development + '/static/css'
 		},
 
 		// Css 경로
 		css = {
 			src : source + '/css/**/*.css',
-			// src : source + '/css/**',
-			dest: development + '/static/css/renew'
+			dest: development + '/static/css'
 		},
 
 		// JS 경로
 		js = {
 			src : source + '/js/**/*.js',
-			// src : source + '/js/**',
-			dest: development + '/static/js/renew'
+			dest: development + '/static/js'
 		},
 
-		// HTML Prettify 옵션
-		htmlPrettify = {
-			"indent_char": " ",
-			"indent_size": 4
+		// Img 경로
+		img = {
+			// src : source + '/assets/img/**/*.{gif,jpg,png,ico}',
+			src: source + '/img/**/!(sprite)*/*',
+			src_sprite: source + '/img/**/sprite*/*',
+			dest: development + '/img',
+		},
+
+		// etc 경로
+		etc = {
+			src: source + '/!(css|scss|js|img)/**',
+			dest: development + '/static',
+		},
+
+		// HTML 옵션
+		htmlbeautify = {
+			"indentSize": 4
 		};
 
 	return {
-		del  : remove,
-		src  : source,
-		dev  : development,
-		
-		template : template,
-		sass : sass,
-		js   : js,
+		del: remove,
+		src: source,
+		test: test,
+		dev: development,
 
-		htmlPrettify : htmlPrettify
+		template: template,
+		css: css,
+		sass: sass,
+		js: js,
+		img: img,
+		etc: etc,
+
+		htmlbeautify: htmlbeautify
 	};
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,129 +1,238 @@
-'use strict';
-/**
- * 모듈 호출
- */
-var del = require('del'),
-	gulp  = require('gulp'),
-	fileinclude  = require('gulp-file-include'),
-	sass	  = require('gulp-sass'),
-	sourcemaps  = require('gulp-sourcemaps'),
-	autoprefixer  = require('gulp-autoprefixer'),
-	plumber   = require('gulp-plumber'),
-	watch     = require('gulp-watch'),
-	prettify  = require('gulp-html-prettify'),
-	browserSync = require('browser-sync').create(), // browser-sync 호출
+const { src, dest, watch, parallel, series, lastRun } = require('gulp');
+const config = require('./config')();
+const imagemin = require('gulp-imagemin');
+const fileinclude = require('gulp-file-include');
+const htmlbeautify = require('gulp-html-beautify');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const csso = require('gulp-csso');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const spritesmith = require('gulp.spritesmith');
+const replace = require('gulp-replace');
+const copy = require('gulp-copy');
+// const rename = require('gulp-rename');
 
-	config  = require('./config')(); // 환경설정 ./config.js
-	
-	sass.compiler = require('node-sass');
-
-/*
- * Gulp 업무(Tasks) 정의 v3.9.1
- */
-
-// 기본
-gulp.task('default', ['browserSync', 'watch']);
-gulp.task('mobile', ['browserSync_m', 'watch_m']);
-
-gulp.task('browserSync', ['template', 'sass', 'js'], function() {
-	return browserSync.init({
-		server: {
-			baseDir: './dist'
-		}
-	});
-});
-gulp.task('browserSync_m', ['template_m', 'sass', 'js'], function() {
-	return browserSync.init({
-		server: {
-			baseDir: './dist'
-		}
-	});
-});
-
-// PC 관찰
-gulp.task('watch', [], function(){
-	// HTML 템플릿 업무 관찰
-	watch([config.template.src, config.template.parts], function() {
-		gulp.start('template');
-	});
-	// Sass 업무 관찰
-	watch(config.sass.src, function() {
-		gulp.start('sass');
-	});
-	// Js 업무 관찰
-	watch(config.js.src, function() {
-		gulp.start('js');
-	});
-});
-// Mobile 관찰
-gulp.task('watch_m', [], function(){
-	// HTML 템플릿 업무 관찰
-	watch([config.template.src_m, config.template.parts_m], function() {
-		gulp.start('template_m');
-	});
-	// Sass 업무 관찰
-	watch(config.sass.src, function() {
-		gulp.start('sass');
-	});
-	// Js 업무 관찰
-	watch(config.js.src, function() {
-		gulp.start('js');
-	});
-});
-
-// 제거
-gulp.task('clean:all', function(){
-	del(config.dev);
-});
-gulp.task('clean:css', function(){
-	del(config.sass.dest);
-});
-gulp.task('clean:js', function(){
-	del(config.js.dest);
-});
-
-//20190611 return 제거
-// HTML 템플릿
-gulp.task('template', function(){
-	gulp.src(config.template.src)
-		.pipe( plumber() )
-		.pipe( fileinclude({
-			prefix: '@@',
-			basepath: '@file'
-		}))
-		.pipe( prettify( config.htmlPrettify) )
-		.pipe( gulp.dest( config.template.dest) )
-		.pipe(browserSync.stream({ match: '**/*.html' }));
-});
-gulp.task('template_m', function(){
-	gulp.src(config.template.src_m)
-		.pipe( plumber() )
-		.pipe( fileinclude({
-			prefix: '@@',
-			basepath: '@file'
-		}))
-		.pipe( prettify( config.htmlPrettify) )
-		.pipe( gulp.dest( config.template.dest_m) )
-		.pipe(browserSync.stream({ match: '**/*.html' }));
-});
-// scss 컴파일러
-gulp.task('sass', function() {
-	gulp.src( config.sass.src )
-		.pipe( plumber() )
-		.pipe(sourcemaps.init())
-		.pipe( sass({outputStyle: 'compact'}).on('error', sass.logError)) // {outputStyle: nested} expanded, compact, compressed
-        .pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-            cascade: false
+function bSync() {
+    browserSync.init({
+        // watch: true,
+        port: 3030,
+        startPath: '/views/main',
+        server: {
+            baseDir: './dist'
+        }
+    });
+};
+function bSyncTest() {
+    browserSync.init({
+        // watch: true,
+        port: 3050,
+        startPath: '/views/main',
+        server: {
+            baseDir: './dist'
+        }
+    });
+};
+function template() {
+    return src(config.template.src, { since: lastRun(template) })
+        .pipe(fileinclude({
+            prefix: '@@',
+            // basepath: 'src'
+            basepath: '@file'
+            // basepath: '@root'
         }))
-		.pipe(sourcemaps.write())
-		.pipe( gulp.dest( config.sass.dest) )
-		.pipe(browserSync.stream({ match: '**/*.css' }));
-});
-// js reload
-gulp.task('js', function(){
-	gulp.src(config.js.src)
-		.pipe( plumber() )
-		.pipe( gulp.dest(config.js.dest) )
-		.pipe(browserSync.stream({ match: '**/*.js' }));
-});
+        .pipe(htmlbeautify(config.htmlbeautify))
+        .pipe(dest(config.template.dest))
+        .pipe(browserSync.stream({ match: '**/*.html' }));
+};
+function templateAll() {
+    return src(config.template.src)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+            // basepath: '@root'
+        }))
+        .pipe(htmlbeautify(config.htmlbeautify))
+        .pipe(dest(config.template.dest))
+        .pipe(browserSync.stream({ match: '**/*.html' }));
+};
+function templateM() {
+    return src(config.template.src_m, { since: lastRun(template) })
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+            // basepath: '@root'
+        }))
+        .pipe(htmlbeautify(config.htmlbeautify))
+        .pipe(dest(config.template.dest_m))
+        .pipe(browserSync.stream({ match: '**/*.html' }));
+};
+function templateMAll() {
+    return src(config.template.src_m)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+            // basepath: '@root'
+        }))
+        .pipe(htmlbeautify(config.htmlbeautify))
+        .pipe(dest(config.template.dest_m))
+        .pipe(browserSync.stream({ match: '**/*.html' }));
+};
+// {outputStyle: nested} expanded, compact, compressed
+function sassDev() {
+    return src(config.sass.src, { since: lastRun(sassDev), sourcemaps: true })
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(dest(config.sass.dest, { sourcemaps: true }))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+};
+function sassDevAll() {
+    return src(config.sass.src, { sourcemaps: true })
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(dest(config.sass.dest, { sourcemaps: true }))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+};
+function sassPrd() {
+    return src(config.sass.src, { since: lastRun(sassPrd) })
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(dest(config.sass.dest))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+};
+function sassPrdAll() {
+    return src(config.sass.src)
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(dest(config.sass.dest))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+};
+function css() {
+    return src(config.css.src, { since: lastRun(css) })
+        // .pipe(autoprefixer())
+        // .pipe(csso())
+        // .pipe(rename({ suffix: '.min' }))
+        .pipe(dest(config.css.dest))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
+};
+function js() {
+    return src(config.js.src, { since: lastRun(js) })
+        .pipe(dest(config.js.dest))
+        .pipe(browserSync.stream({ match: '**/*.js' }));
+};
+function img() {
+    return src(config.img.src, { since: lastRun(img) })
+        .pipe(imagemin())
+        .pipe(dest(config.img.dest));
+}
+function sprite(cb) {
+    // var spriteData = src('src/img/sprite/login/*') // custom grouping
+    var spriteData = src(config.img.src_sprite)
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            padding: 6,
+            cssName: 'sprite.css'
+        }));
+    spriteData.img.pipe(dest(config.img.dest + '/sprite'));
+    // spriteData.css.pipe(dest(config.sass.dest + '/sprite'));
+    spriteData.css.pipe(dest(config.sass.src_sprite)); // 생성 후 scss로 변경
+    cb();
+};
+function etc() {
+    return src(config.etc.src, { since: lastRun(etc) })
+        .pipe(dest(config.etc.dest));
+}
+function watchingDev() {
+    watch([config.template.src], template);
+    watch([config.template.parts, config.src + '/template/layer/**'], templateAll);
+    watch([config.template.src_m], templateM);
+    watch([config.template.parts_m], templateMAll);
+    watch(config.sass.src, sassDev)
+    watch(config.sass.parts, sassDevAll)
+    watch(config.css.src, css)
+    watch(config.js.src, js)
+    watch(config.img.src, img);
+    watch(config.etc.src, etc);
+};
+function watchingPrd() {
+    watch([config.template.src], template);
+    watch([config.template.parts, config.src + '/template/layer/**'], templateAll);
+    watch([config.template.src_m], templateM);
+    watch([config.template.parts_m], templateMAll);
+    watch(config.sass.src, sassPrd)
+    watch(config.sass.parts, sassPrdAll)
+    watch(config.css.src, css)
+    watch(config.js.src, js)
+    watch(config.img.src, img);
+    watch(config.etc.src, etc);
+};
+
+// util
+function cleanDist(cb) {
+    del(config.dev);
+    cb();
+};
+function cleanTemplate(cb) {
+    del([config.template.dest, config.template.dest_m]);
+    // del([config.template.dest]);
+    cb();
+};
+function cleanParts(cb) {
+    del([config.template.dest_parts, config.template.dest_m_parts]);
+    // del([config.template.dest_parts]);
+    cb();
+};
+function cleanSass(cb) {
+    del(config.sass.dest);
+    cb();
+};
+function cleanCss(cb) {
+    del(config.css.dest);
+    cb();
+};
+function cleanJs(cb) {
+    del(config.js.dest);
+    cb();
+};
+function cleanImg(cb) {
+    del(config.img.dest);
+    cb();
+};
+
+const assets_local = '/public';
+const assets_server = 'http://toxnsldxn.cafe24.com/template/assets';
+const views_local = '/views';
+const views_server = 'http://toxnsldxn.cafe24.com/template/views';
+
+function copyTest() {
+    return src(config.dev + '/**/*')
+        .pipe(dest(config.test));
+};
+function testPathServer() {
+    return src(config.test + '/**/*')
+        .pipe(replace(assets_local, assets_server))
+        .pipe(replace(views_local, views_server))
+        .pipe(dest(config.test));
+};
+function testPathLocal() {
+    return src(config.test + '/**/*')
+        .pipe(replace(assets_server, assets_local))
+        .pipe(replace(views_server, views_local))
+        .pipe(dest(config.test));
+};
+
+exports.cleanTemplate = cleanTemplate;
+exports.cleanDist = cleanDist;
+exports.cleanParts = cleanParts;
+exports.img = img;
+exports.sprite = sprite;
+exports.etc = etc;
+exports.testPathServer = testPathServer;
+exports.testPathLocal = testPathLocal;
+exports.serve = parallel(series(parallel(template, templateM), sassDev, js, img, etc, bSync), watchingDev);
+exports.build = parallel(series(parallel(template, templateM), sassPrd, js, img, etc, bSync), watchingPrd);
+exports.default = parallel(bSync, watchingPrd);
+exports.test = series(copyTest, testPathServer, bSyncTest);
